6c3fa9d1d27c3fc44f1e6b451c1c42f25dbf269b
Sam <kooldude5353@gmail.com>
2022-04-09 12:28:20 -0500

    Initial commit

---
+++ src/main/java/net/TheSquirrel172/netcraft/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.TheSquirrel172.netcraft.mixin;
+
+import net.TheSquirrel172.netcraft.NetCraft;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		NetCraft.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
